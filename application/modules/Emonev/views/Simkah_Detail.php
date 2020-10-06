f7a197e018a26ee1e8b9e563c755fa3c45f8685d
maspriyambodo <maspriyambodo@gmail.com>
2020-10-06 09:37:57 +0700

    update table Simkah

--- application/modules/Emonev/views/Simkah_Detail.php
+++ application/modules/Emonev/views/Simkah_Detail.php
@@ -60,9 +60,9 @@
             "scrollCollapse": true,
             "scrollX": true,
             "scrollY": "400px",
-            dom: `<'row'<'col-sm-6 text-left'l><'col-sm-6 text-right'B>>
+            dom: `<'row'<'col-sm-6 text-left'f><'col-sm-6 text-right'B>>
 <'row'<'col-sm-12'tr>>
-<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7 dataTables_pager'p>>`,
+<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7 dataTables_pager'lp>>`,
             buttons: [
                 'print',
                 'copyHtml5',
