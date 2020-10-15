dbc9073559c0959a15ff9207cab5ddf6bec82e99
maspriyambodo <maspriyambodo@gmail.com>
2020-10-15 15:10:37 +0700

    add module Emonev->Pensiun

--- application/modules/Sekertariat/controllers/Pensiun/Pensiun.php
+++ application/modules/Sekertariat/controllers/Pensiun/Pensiun.php
@@ -37,21 +37,34 @@ class Pensiun extends CI_Controller {
             'title' => 'Data Pensiun Provinsi ' . $param[1] . ' | RUDABI SYSTEM OF KEMENAG RI',
             'username' => $this->session->userdata('username'),
             'param' => $param,
-            'data' => read_file('https://simas.kemenag.go.id/rudabi/datapi/sicakepp/pensiun??KEY=BOBA&peg_provinsi=' . $param[0])
+            'data' => read_file('https://simas.kemenag.go.id/rudabi/datapi/sicakepp/pensiun?KEY=BOBA&peg_provinsi=' . $param[0]),
+            'gol' => read_file('https://simas.kemenag.go.id/rudabi/datapi/sicakepp/golongan?KEY=BOBA')
         ];
         $data['content'] = $this->parser->parse('Pensiun/Provinsi', $data, true);
         return $this->parser->parse('Dashboard/Template', $data);
     }
-    
-    public function Golongan() {
-        $param = $this->bodo->Url($this->input->post_get('key')); // output $param = Array ( [0] => 17 [1] => Banten )
+
+    public function Kabupaten() {
+        $param = $this->bodo->Url($this->input->post_get('key')); // output $param = Array ( [0] => 126 as kab_id [1] => Jakarta Pusat as kab_nama [2] => 12 as peg_provinsi [3] => DKI Jakarta as propinsi_nama ) 
         $data = [
-            'title' => 'Data Pensiun Provinsi ' . $param[1] . ' | RUDABI SYSTEM OF KEMENAG RI',
+            'title' => 'Data Pensiun Kabupaten ' . $param[1] . ' | RUDABI SYSTEM OF KEMENAG RI',
             'username' => $this->session->userdata('username'),
             'param' => $param,
-            'data' => read_file('https://simas.kemenag.go.id/rudabi/datapi/sicakepp/pensiun??KEY=BOBA&peg_provinsi=' . $param[0])
+            'data' => read_file('https://simas.kemenag.go.id/rudabi/datapi/sicakepp/pensiun?KEY=boba&kab_id=' . $param[0])
         ];
-        $data['content'] = $this->parser->parse('Pensiun/Provinsi', $data, true);
+        $data['content'] = $this->parser->parse('Pensiun/V_Kabupaten', $data, true);
+        return $this->parser->parse('Dashboard/Template', $data);
+    }
+
+    public function Golongan() {
+        $param = $this->bodo->Url($this->input->post_get('key')); // output $param = Array ( [0] => 126 as kab_id [1] => Jakarta Pusat as kab_nama [2] => 217 as item_id [3] => I/a - Juru Muda as item_name [4] => 12 as peg_provinsi [5] => DKI Jakarta as propinsi_nama )
+        $data = [
+            'title' => 'Detail Pegawai Golongan ' . $param[3] . ' | RUDABI SYSTEM OF KEMENAG RI',
+            'username' => $this->session->userdata('username'),
+            'param' => $param,
+            'data' => read_file('https://simas.kemenag.go.id/rudabi/datapi/sicakepp/pensiun?KEY=boba&kab_id=' . $param[0] . '&item_id=' . $param[2] . '')
+        ];
+        $data['content'] = $this->parser->parse('Pensiun/Golongan', $data, true);
         return $this->parser->parse('Dashboard/Template', $data);
     }
 
