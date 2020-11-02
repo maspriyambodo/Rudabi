cf27d579f25558592144f6c5c0c71efeae50bc23
maspriyambodo <maspriyambodo@gmail.com>
2020-11-02 23:38:33 +0700

    update dashboard

---
+++ application/modules/Dashboard/views/V_Dashboard_orig.php
@@ -0,0 +1,354 @@
+<div class="row">
+    <div class="col-md-6">
+        <div class="card card-custom card-stretch" id="kt_page_stretched_card">
+            <div class="card-header">
+                <div class="card-title">
+                    DIREKTORAT BINA KUA DAN KELUARGA SAKINAH
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="card-scroll">
+                    <div class="d-flex flex-wrap align-items-center mb-10">
+                        <div class="symbol symbol-60 symbol-2by3 flex-shrink-0">
+                            <div class="symbol-label">
+                                <span class="svg-icon menu-icon"> <i class="fas fa-ring"></i> </span>
+                            </div>
+                        </div>
+                        <div class="d-flex flex-column ml-4 flex-grow-1 mr-2">
+                            <a href="#" class="text-dark-75 font-weight-bold text-hover-primary font-size-lg mb-1">
+                                BIMWIN
+                            </a>
+                            <span class="text-muted font-weight-bold">Bimbingan Perkawinan</span>
+                        </div>
+                    </div>
+                    <div class="d-flex flex-wrap align-items-center mb-10">
+                        <div class="symbol symbol-60 symbol-2by3 flex-shrink-0">
+                            <div class="symbol-label">
+                                <span class="svg-icon menu-icon"> <i class="fas fa-chalkboard-teacher"></i> </span>
+                            </div>
+                        </div>
+                        <div class="d-flex flex-column ml-4 flex-grow-1 mr-2">
+                            <a href="#" class="text-dark-75 font-weight-bold text-hover-primary font-size-lg mb-1">
+                                e-MONEV
+                            </a>
+                            <span class="text-muted font-weight-bold">Sisem Aplikasi Monitoring KUA</span>
+                        </div>
+                    </div>
+                    <div class="d-flex flex-wrap align-items-center mb-10">
+                        <div class="symbol symbol-60 symbol-2by3 flex-shrink-0">
+                            <div class="symbol-label">
+                                <span class="svg-icon menu-icon"> <i class="fas fa-genderless"></i> </span>
+                            </div>
+                        </div>
+                        <div class="d-flex flex-column ml-4 flex-grow-1 mr-2">
+                            <a href="#" class="text-dark-75 font-weight-bold text-hover-primary font-size-lg mb-1">
+                                SIK
+                            </a>
+                            <span class="text-muted font-weight-bold">Sistem Informasi Kepenghuluan</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-6">
+        <div class="card card-custom card-stretch" id="kt_page_stretched_card">
+            <div class="card-header">
+                <div class="card-title">
+                    DIREKTORAT PEMBERDAYAAN ZAKAT DAN WAKAF
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="card-scroll">
+                    <div class="d-flex flex-wrap align-items-center mb-10">
+                        <div class="symbol symbol-60 symbol-2by3 flex-shrink-0">
+                            <div class="symbol-label">
+                                <span class="svg-icon menu-icon"> <i class="fas fa-hand-holding-usd"></i> </span>
+                            </div>
+                        </div>
+                        <div class="d-flex flex-column ml-4 flex-grow-1 mr-2">
+                            <a href="#" class="text-dark-75 font-weight-bold text-hover-primary font-size-lg mb-1">
+                                SIMZAT
+                            </a>
+                            <span class="text-muted font-weight-bold">Sistem Informasi Zakat</span>
+                        </div>
+                    </div>
+                    <div class="d-flex flex-wrap align-items-center mb-10">
+                        <div class="symbol symbol-60 symbol-2by3 flex-shrink-0">
+                            <div class="symbol-label">
+                                <span class="svg-icon menu-icon"> <i class="fas fa-hand-holding-heart"></i> </span>
+                            </div>
+                        </div>
+                        <div class="d-flex flex-column ml-4 flex-grow-1 mr-2">
+                            <a href="#" class="text-dark-75 font-weight-bold text-hover-primary font-size-lg mb-1">
+                                SIWAK
+                            </a>
+                            <span class="text-muted font-weight-bold">Sistem Informasi Wakaf</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="clear" style="margin:5% 0px;"></div>
+<div class="row">
+    <div class="col-md-6">
+        <div class="card card-custom card-stretch" id="kt_page_stretched_card">
+            <div class="card-header">
+                <div class="card-title">
+                    DIREKTORAT PENERANGAN AGAMA ISLAM
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="card-scroll">
+                    <div class="d-flex flex-wrap align-items-center mb-10">
+                        <div class="symbol symbol-60 symbol-2by3 flex-shrink-0">
+                            <div class="symbol-label">
+                                <span class="svg-icon menu-icon"> <i class="fas fa-book-reader"></i> </span>
+                            </div>
+                        </div>
+                        <div class="d-flex flex-column ml-4 flex-grow-1 mr-2">
+                            <a href="#" class="text-dark-75 font-weight-bold text-hover-primary font-size-lg mb-1">
+                                e-MTQ
+                            </a>
+                            <span class="text-muted font-weight-bold">Sistem Aplikasi Pendaftaran MTQ</span>
+                        </div>
+                    </div>
+                    <div class="d-flex flex-wrap align-items-center mb-10">
+                        <div class="symbol symbol-60 symbol-2by3 flex-shrink-0">
+                            <div class="symbol-label">
+                                <span class="svg-icon menu-icon"> <i class="fas fa-bullhorn"></i> </span>
+                            </div>
+                        </div>
+                        <div class="d-flex flex-column ml-4 flex-grow-1 mr-2">
+                            <a href="#" class="text-dark-75 font-weight-bold text-hover-primary font-size-lg mb-1">
+                                e-PAI
+                            </a>
+                            <span class="text-muted font-weight-bold">e-Kinerja Penyuluh Agama Islam</span>
+                        </div>
+                    </div>
+                    <div class="d-flex flex-wrap align-items-center mb-10">
+                        <div class="symbol symbol-60 symbol-2by3 flex-shrink-0">
+                            <div class="symbol-label">
+                                <span class="svg-icon menu-icon"> <i class="far fa-lightbulb"></i> </span>
+                            </div>
+                        </div>
+                        <div class="d-flex flex-column ml-4 flex-grow-1 mr-2">
+                            <a href="#" class="text-dark-75 font-weight-bold text-hover-primary font-size-lg mb-1">
+                                SIMPENAIS
+                            </a>
+                            <span class="text-muted font-weight-bold">Sistem Penerangan Agama Islam</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-6">
+        <div class="card card-custom card-stretch" id="kt_page_stretched_card">
+            <div class="card-header">
+                <div class="card-title">
+                    DIREKTORAT URUSAN AGAMA ISLAM dan PEMBINAAN SYARIAH
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="card-scroll">
+                    <div class="d-flex flex-wrap align-items-center mb-10">
+                        <div class="symbol symbol-60 symbol-2by3 flex-shrink-0">
+                            <div class="symbol-label">
+                                <span class="svg-icon menu-icon"> <i class="fas fa-toolbox"></i> </span>
+                            </div>
+                        </div>
+                        <div class="d-flex flex-column ml-4 flex-grow-1 mr-2">
+                            <a href="#" class="text-dark-75 font-weight-bold text-hover-primary font-size-lg mb-1">
+                                SIHAT
+                            </a>
+                            <span class="text-muted font-weight-bold">Sistem Informasi Hisab Rukyat</span>
+                        </div>
+                    </div>
+                    <div class="d-flex flex-wrap align-items-center mb-10">
+                        <div class="symbol symbol-60 symbol-2by3 flex-shrink-0">
+                            <div class="symbol-label">
+                                <span class="svg-icon menu-icon"> <i class="fas fa-mosque"></i> </span>
+                            </div>
+                        </div>
+                        <div class="d-flex flex-column ml-4 flex-grow-1 mr-2">
+                            <a href="#" class="text-dark-75 font-weight-bold text-hover-primary font-size-lg mb-1">
+                                SIMAS
+                            </a>
+                            <span class="text-muted font-weight-bold">Sistem Informasi Masjid</span>
+                        </div>
+                    </div>
+                    <div class="d-flex flex-wrap align-items-center mb-10">
+                        <div class="symbol symbol-60 symbol-2by3 flex-shrink-0">
+                            <div class="symbol-label">
+                                <span class="svg-icon menu-icon"> <i class="fas fa-moon"></i> </span>
+                            </div>
+                        </div>
+                        <div class="d-flex flex-column ml-4 flex-grow-1 mr-2">
+                            <a href="#" class="text-dark-75 font-weight-bold text-hover-primary font-size-lg mb-1">
+                                SIPAHAM
+                            </a>
+                            <span class="text-muted font-weight-bold">Sistem Informasi Paham Keagamaan</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="clear" style="margin:5% 0px;"></div>
+<div class="row">
+    <div class="col-md-6">
+        <div class="card card-custom card-stretch" id="kt_page_stretched_card">
+            <div class="card-header">
+                <div class="card-title">
+                    SEKERTARIAT
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="card-scroll">
+                    <div class="d-flex flex-wrap align-items-center mb-10">
+                        <div class="symbol symbol-60 symbol-2by3 flex-shrink-0">
+                            <div class="symbol-label">
+                                <span class="svg-icon menu-icon"> <i class="fas fa-file-invoice-dollar"></i> </span>
+                            </div>
+                        </div>
+                        <div class="d-flex flex-column ml-4 flex-grow-1 mr-2">
+                            <a href="#" class="text-dark-75 font-weight-bold text-hover-primary font-size-lg mb-1">
+                                e-SBSN
+                            </a>
+                            <span class="text-muted font-weight-bold">e-Planning Surat Berharga Syariah Negara</span>
+                        </div>
+                    </div>
+                    <div class="d-flex flex-wrap align-items-center mb-10">
+                        <div class="symbol symbol-60 symbol-2by3 flex-shrink-0">
+                            <div class="symbol-label">
+                                <span class="svg-icon menu-icon"> <i class="fas fa-clipboard-list"></i> </span>
+                            </div>
+                        </div>
+                        <div class="d-flex flex-column ml-4 flex-grow-1 mr-2">
+                            <a href="#" class="text-dark-75 font-weight-bold text-hover-primary font-size-lg mb-1">
+                                EBI
+                            </a>
+                            <span class="text-muted font-weight-bold">e-Planning Bimas Islam</span>
+                        </div>
+                    </div>
+                    <div class="d-flex flex-wrap align-items-center mb-10">
+                        <div class="symbol symbol-60 symbol-2by3 flex-shrink-0">
+                            <div class="symbol-label">
+                                <span class="svg-icon menu-icon"> <i class="fas fa-chart-line"></i> </span>
+                            </div>
+                        </div>
+                        <div class="d-flex flex-column ml-4 flex-grow-1 mr-2">
+                            <a href="#" class="text-dark-75 font-weight-bold text-hover-primary font-size-lg mb-1">
+                                SICAKEP
+                            </a>
+                            <span class="text-muted font-weight-bold">Sistem Informasi Capaian Kinerja</span>
+                        </div>
+                    </div>
+                    <div class="d-flex flex-wrap align-items-center mb-10">
+                        <div class="symbol symbol-60 symbol-2by3 flex-shrink-0">
+                            <div class="symbol-label">
+                                <span class="svg-icon menu-icon"> <i class="fas fa-book"></i> </span>
+                            </div>
+                        </div>
+                        <div class="d-flex flex-column ml-4 flex-grow-1 mr-2">
+                            <a href="#" class="text-dark-75 font-weight-bold text-hover-primary font-size-lg mb-1">
+                                SILP
+                            </a>
+                            <span class="text-muted font-weight-bold">Sistem Informasi Laporan Pimpinan</span>
+                        </div>
+                    </div>
+                    <div class="d-flex flex-wrap align-items-center mb-10">
+                        <div class="symbol symbol-60 symbol-2by3 flex-shrink-0">
+                            <div class="symbol-label">
+                                <span class="svg-icon menu-icon"> <i class="fas fa-cogs"></i> </span>
+                            </div>
+                        </div>
+                        <div class="d-flex flex-column ml-4 flex-grow-1 mr-2">
+                            <a href="#" class="text-dark-75 font-weight-bold text-hover-primary font-size-lg mb-1">
+                                SIMBI
+                            </a>
+                            <span class="text-muted font-weight-bold">Sistem Informasi Manajemen Bimas Islam</span>
+                        </div>
+                    </div>
+                    <div class="d-flex flex-wrap align-items-center mb-10">
+                        <div class="symbol symbol-60 symbol-2by3 flex-shrink-0">
+                            <div class="symbol-label">
+                                <span class="svg-icon menu-icon"> <i class="fas fa-mail-bulk"></i> </span>
+                            </div>
+                        </div>
+                        <div class="d-flex flex-column ml-4 flex-grow-1 mr-2">
+                            <a href="#" class="text-dark-75 font-weight-bold text-hover-primary font-size-lg mb-1">
+                                SIPBI
+                            </a>
+                            <span class="text-muted font-weight-bold">Sistem Informasi Persuratan Bimas Islam</span>
+                        </div>
+                    </div>
+                    <div class="d-flex flex-wrap align-items-center mb-10">
+                        <div class="symbol symbol-60 symbol-2by3 flex-shrink-0">
+                            <div class="symbol-label">
+                                <span class="svg-icon menu-icon"> <i class="fas fa-user-shield"></i> </span>
+                            </div>
+                        </div>
+                        <div class="d-flex flex-column ml-4 flex-grow-1 mr-2">
+                            <a href="#" class="text-dark-75 font-weight-bold text-hover-primary font-size-lg mb-1">
+                                Cyber
+                            </a>
+                            <span class="text-muted font-weight-bold">Anti Narkoba, Pornografi, dan Radikalisme</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+    var KTLayoutStretchedCard = function () {
+        var _element;
+        var _init = function () {
+            var scroll = KTUtil.find(_element, '.card-scroll');
+            var cardBody = KTUtil.find(_element, '.card-body');
+            var cardHeader = KTUtil.find(_element, '.card-header');
+
+            var height = KTLayoutContent.getHeight();
+
+            height = height - parseInt(KTUtil.actualHeight(cardHeader));
+
+            height = height - parseInt(KTUtil.css(_element, 'marginTop')) - parseInt(KTUtil.css(_element, 'marginBottom'));
+            height = height - parseInt(KTUtil.css(_element, 'paddingTop')) - parseInt(KTUtil.css(_element, 'paddingBottom'));
+
+            height = height - parseInt(KTUtil.css(cardBody, 'paddingTop')) - parseInt(KTUtil.css(cardBody, 'paddingBottom'));
+            height = height - parseInt(KTUtil.css(cardBody, 'marginTop')) - parseInt(KTUtil.css(cardBody, 'marginBottom'));
+
+            height = height - 3;
+
+            KTUtil.css(scroll, 'height', height + 'px');
+        };
+        return {
+            init: function (id) {
+                _element = KTUtil.getById(id);
+
+                if (!_element) {
+                    return;
+                }
+                _init();
+                KTUtil.addResizeHandler(function () {
+                    _init();
+                }
+                );
+            },
+
+            update: function () {
+                _init();
+            }
+        };
+    }();
+    if (typeof module !== 'undefined') {
+        module.exports = KTLayoutStretchedCard;
+    }
+
+
+</script>
