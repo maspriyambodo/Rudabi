92dece61401619ba7720096c53cbb29fb4aaa3df
maspriyambodo <maspriyambodo@gmail.com>
2020-11-03 20:16:14 +0700

    adding modules Siwak

--- application/modules/PAI/views/V_Seniman.php
+++ application/modules/PAI/views/V_Seniman.php
@@ -64,6 +64,16 @@
             "scrollCollapse": true,
             "scrollX": true,
             "scrollY": "400px",
+            dom: `<'row'<'col-sm-6 text-left'f><'col-sm-6 text-right'B>>
+                <'row'<'col-sm-12'tr>>
+                <'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7 dataTables_pager'lp>>`,
+            buttons: [
+                'print',
+                'copyHtml5',
+                'excelHtml5',
+                'csvHtml5',
+                'pdfHtml5'
+            ],
             "ajax": {
                 dataSrc: '',
                 method: "GET",
