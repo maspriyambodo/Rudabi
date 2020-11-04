8e332f56e0c2c4a1eaef079e2555a56a78a4eee4
maspriyambodo <maspriyambodo@gmail.com>
2020-11-04 11:06:23 +0700

    update footer credit template

--- application/modules/Dashboard/views/Template.php
+++ application/modules/Dashboard/views/Template.php
@@ -680,7 +680,7 @@
                     </div>
                     <div class="footer bg-white py-4 d-flex flex-lg-column" id="kt_footer">
                         <div class="container-fluid d-flex flex-column flex-md-row align-items-center justify-content-between">
-                            <div class="text-dark order-2 order-md-1"><span class="text-muted font-weight-bold mr-2"><?= $this->bodo->Since(); ?> &copy;</span><a href="https://kemenag.go.id" target="_blank" class="text-dark-75 text-hover-primary">RUDABI | Direktorat Bimas Islam</a></div>
+                            <div class="text-dark order-2 order-md-1"><span class="text-muted font-weight-bold mr-2"><?= $this->bodo->Since(); ?> &copy;</span><a href="https://kemenag.go.id" target="_blank" class="text-dark-75 text-hover-primary">RUDABI | Ditjen Bimas Islam</a></div>
                             <div class="nav nav-dark"> </div>
                         </div>
                     </div>
