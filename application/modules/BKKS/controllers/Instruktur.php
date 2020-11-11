74d775b0b0b0b7f958ffe42c4c6da1ee47742ece
maspriyambodo <maspriyambodo@gmail.com>
2020-11-11 11:02:26 +0700

    add module BKKS->Fasilitator

---
+++ application/modules/BKKS/controllers/Instruktur.php
@@ -0,0 +1,44 @@
+<?php
+
+defined('BASEPATH')OR exit('No direct script access allowed');
+/*
+ * Product:        System of kementerian agama Republik Indonesia
+ * License Type:   Government
+ * Access Type:    Multi-User
+ * License:        https://maspriyambodo.com
+ * maspriyambodo@gmail.com
+ * 
+ * Thank you,
+ * maspriyambodo
+ */
+
+/**
+ * Description of Instruktur
+ *
+ * @author centos
+ */
+class Instruktur extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('M_Bimwin');
+        $this->Authentication = $this->M_Bimwin->Auth();
+    }
+
+    private function Dec($enc) {
+        $encrypt = str_replace(['-', '_', '~'], ['+', '/', '='], $enc);
+        $dec = $this->encryption->decrypt($encrypt);
+        return $dec;
+    }
+
+    public function index() {
+        $data = [
+            'title' => 'Data Instruktur | RUDABI SYSTEM OF KEMENAG RI',
+            'username' => $this->Authentication[0]->uname,
+            'data' => read_file('https://simas.kemenag.go.id/rudabi/datapi/embimwin/instruktur?KEY=boba')
+        ];
+        $data['content'] = $this->parser->parse('Instruktur/index', $data, true);
+        return $this->parser->parse('Dashboard/Template', $data);
+    }
+
+}
---
+++ application/modules/BKKS/views/Instruktur/index.php
@@ -0,0 +1,31 @@
+<div class="subheader py-2 py-lg-4 subheader-solid" id="kt_subheader">
+    <div class="container-fluid d-flex align-items-center justify-content-between flex-wrap flex-sm-nowrap">
+        <div class="d-flex align-items-center flex-wrap mr-2">
+            <h5 class="text-dark font-weight-bold mt-2 mb-2 mr-5">Data Instruktur</h5>
+        </div>
+    </div>
+</div>
+<div class="card card-custom" data-card="true" id="kt_card_1">
+    <div class="card-header">
+        <div class="card-title">
+            Data Instruktur Bimwin
+        </div>
+        <div class="card-toolbar">
+            <a href="#" class="btn btn-icon btn-sm btn-hover-light-primary mr-1" data-card-tool="toggle" data-toggle="tooltip" data-placement="top" title="Minimalkan">
+                <i class="ki ki-arrow-down icon-nm"></i>
+            </a>
+        </div>
+    </div>
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-bordered table-hover table-striped" style="width:100%;">
+                <thead class="text-center text-uppercase">
+                    <tr>
+                        <th>no</th>
+                        <th>no</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
+</div>
