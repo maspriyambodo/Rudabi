14f9cecaad9c61b29752545abfff856d30961f4c
maspriyambodo <maspriyambodo@gmail.com>
2020-11-11 11:46:30 +0700

    fixing error

--- application/modules/Dashboard/views/Template.php
+++ application/modules/Dashboard/views/Template.php
@@ -287,7 +287,7 @@
                                                 </a>
                                             </li>
                                             <li class="menu-item menu-item" aria-haspopup="true" data-menu-toggle="hover">
-                                                <a href="<?= base_url('KUA/Catin/index/' . date("Y") . ''); ?>" class="menu-link menu-toggle">
+                                                <a href="<?= base_url('BKKS/Catin/index'); ?>" class="menu-link menu-toggle">
                                                     <i class="menu-bullet menu-bullet-dot">
                                                         <span></span>
                                                     </i>
