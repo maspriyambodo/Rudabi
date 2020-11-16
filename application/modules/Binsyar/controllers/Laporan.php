fbfdec51611543755b073b6544212100ed47cc5b
maspriyambodo <maspriyambodo@gmail.com>
2020-11-16 11:41:49 +0700

    update Sihat->Laporan

--- application/modules/Binsyar/controllers/Laporan.php
+++ application/modules/Binsyar/controllers/Laporan.php
@@ -28,9 +28,22 @@ class Laporan extends CI_Controller {
     public function index() {
         $data = [
             'title' => 'Hisab Laporan| RUDABI SYSTEM OF KEMENAG RI',
-            'username' => $this->Authentication[0]->uname
+            'username' => $this->Authentication[0]->uname,
+            'data' => read_file('https://simas.kemenag.go.id/rudabi/datapi/siihat/hisablaporan?KEY=BOBA')
         ];
         $data['content'] = $this->parser->parse('Binsyar/V_laporan', $data, true);
+        return $this->parser->parse('Dashboard/Template', $data);
+    }
+
+    public function Provinsi() {
+        $param = $this->bodo->Url($this->input->post_get('key')); // output $param = Array ( [0] => 12 as ukur_provinsi [1] => Jawa Barat as province_title)
+        $data = [
+            'title' => 'Data Hisab Provinsi ' . $param[1] . '| RUDABI SYSTEM OF KEMENAG RI',
+            'username' => $this->Authentication[0]->uname,
+            'data' => read_file('https://simas.kemenag.go.id/rudabi/datapi/siihat/hisablaporan?KEY=BOBA&ukur_provinsi=' . $param[0]),
+            'param' => $param
+        ];
+        $data['content'] = $this->parser->parse('Binsyar/Laporan_Provinsi', $data, true);
         return $this->parser->parse('Dashboard/Template', $data);
     }
 
