452b5359bb783f8251e97e67a6ae8373b609a1ef
maspriyambodo <maspriyambodo@gmail.com>
2021-01-11 15:55:12 +0700

    add module users/urais/simas

---
+++ application/modules/Users/controllers/Binsyar/Tipologi.php
@@ -0,0 +1,48 @@
+<?php
+
+defined('BASEPATH')OR exit('No direct script access allowed');
+/*
+ * Product:        System of kementerian agama Republik Indonesia
+ * License Type:   Government
+ * Access Type:    Multi-User
+ * License:        https://maspriyambodo.com
+ * maspriyambodo@gmail.com
+ * 
+ * Thank you,
+ * maspriyambodo
+ */
+
+/**
+ * Description of Tipologi
+ *
+ * @author centos
+ */
+class Tipologi extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('Binsyar/M_Binsyar');
+        $this->Authentication = $this->M_Binsyar->Auth();
+    }
+
+    public function index() {
+        $data = [
+            'title' => 'Masjid Tipologi | RUDABI SYSTEM OF KEMENAG RI',
+            'username' => $this->Authentication[0]->uname,
+            'data' => read_file('https://simas.kemenag.go.id/rudabi/datapi/eimas/tipologimasjid?KEY=boba')
+        ];
+        $data['content'] = $this->parser->parse('Users/u_urais/Tipologi_masjid', $data, true);
+        return $this->parser->parse('Users/u_urais/Template', $data);
+    }
+
+    public function Mushalla() {
+        $data = [
+            'title' => 'Mushalla Tipologi | RUDABI SYSTEM OF KEMENAG RI',
+            'username' => $this->Authentication[0]->uname,
+            'data' => read_file('https://simas.kemenag.go.id/rudabi/datapi/eimas/tipologimushalla?KEY=boba')
+        ];
+        $data['content'] = $this->parser->parse('Users/u_urais/Tipologi_mushalla', $data, true);
+        return $this->parser->parse('Users/u_urais/Template', $data);
+    }
+
+}
