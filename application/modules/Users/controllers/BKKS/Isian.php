5c23f23e362a229a9e82b11010c249d60e48404f
maspriyambodo <maspriyambodo@gmail.com>
2021-01-12 18:59:50 +0700

    add module Users/BKKS/Bangunan

---
+++ application/modules/Users/controllers/BKKS/Isian.php
@@ -0,0 +1,39 @@
+<?php
+
+defined('BASEPATH')OR exit('No direct script access allowed');
+/*
+ * Product:        System of kementerian agama Republik Indonesia
+ * License Type:   Government
+ * Access Type:    Multi-User
+ * License:        https://maspriyambodo.com
+ * maspriyambodo@gmail.com
+ * 
+ * Thank you,
+ * maspriyambodo
+ */
+
+/**
+ * Description of Isian
+ *
+ * @author centos
+ */
+class Isian extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('Emonev/M_emonev');
+        $this->Authentication = $this->M_emonev->Auth();
+    }
+
+    public function index() {
+        $data = [
+            'title' => 'Rekapitulasi Isian KUA | RUDABI SYSTEM OF KEMENAG RI',
+            'username' => $this->session->userdata('username'),
+            'urung_input' => read_file('https://simas.kemenag.go.id/rudabi/datapi/monev/Rekap?KEY=boba'),
+            'rekap' => read_file('https://simas.kemenag.go.id/rudabi/datapi/monev/Isian?KEY=boba')
+        ];
+        $data['content'] = $this->parser->parse('Users/u_binakua/Isian_index', $data, true);
+        return $this->parser->parse('Users/u_binakua/Template', $data);
+    }
+
+}
