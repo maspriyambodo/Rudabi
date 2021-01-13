92fea3ab36559e0a59af8ef48753a7d1a1217de0
maspriyambodo <maspriyambodo@gmail.com>
2021-01-13 19:53:22 +0700

    add module Users/PAI/qari

---
+++ application/modules/Users/controllers/PAI/Qari.php
@@ -0,0 +1,62 @@
+<?php
+
+defined('BASEPATH')OR exit('No direct script access allowed');
+/*
+ * Product:        System of kementerian agama Republik Indonesia
+ * License Type:   Government
+ * Access Type:    Multi-User
+ * License:        https://maspriyambodo.com
+ * maspriyambodo@gmail.com
+ * 
+ * Thank you,
+ * maspriyambodo
+ */
+
+/**
+ * Description of Guru_ngaji
+ *
+ * @author centos
+ */
+class Qari extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('PAI/M_Simpenais');
+        $this->Authentication = $this->M_Simpenais->Auth();
+    }
+
+    public function index() {
+        $data = [
+            'title' => 'Hafidz & Hafidzah | RUDABI SYSTEM OF KEMENAG RI',
+            'username' => $this->Authentication[0]->uname,
+            'data' => read_file('https://simas.kemenag.go.id/rudabi/datapi/simpenaiss/qari?KEY=BOBA')
+        ];
+        $data['content'] = $this->parser->parse('Users/u_pai/V_qari', $data, true);
+        return $this->parser->parse('Users/u_pai/Template', $data);
+    }
+
+    public function Provinsi() {
+        $param = $this->bodo->Url($this->input->post_get('key')); // output $param = Array ( [0] => 1 as province_id [1] => Nanggroe Aceh Darussalam as province_title)
+        $data = [
+            'title' => 'Qari & Qariah ' . $param[1] . '| RUDABI SYSTEM OF KEMENAG RI',
+            'username' => $this->Authentication[0]->uname,
+            'data' => read_file('https://simas.kemenag.go.id/rudabi/datapi/simpenaiss/qari?KEY=BOBA&province_id=' . $param[0]),
+            'param' => $param
+        ];
+        $data['content'] = $this->parser->parse('Users/u_pai/V_qariprov', $data, true);
+        return $this->parser->parse('Users/u_pai/Template', $data);
+    }
+
+    public function Kabupaten() {
+        $param = $this->bodo->Url($this->input->post_get('key')); // output $param = Array ( [0] => 16 as province_id [1] => Jawa Timur as province_title [2] => 223 as city_id [3] => Kabupaten Kediri as city_title)
+        $data = [
+            'title' => 'Qari & Qariah ' . $param[1] . '| RUDABI SYSTEM OF KEMENAG RI',
+            'username' => $this->Authentication[0]->uname,
+            'data' => read_file('https://simas.kemenag.go.id/rudabi/datapi/simpenaiss/qari?KEY=BOBA&city_id=' . $param[2]),
+            'param' => $param
+        ];
+        $data['content'] = $this->parser->parse('Users/u_pai/V_qarikab', $data, true);
+        return $this->parser->parse('Users/u_pai/Template', $data);
+    }
+
+}
