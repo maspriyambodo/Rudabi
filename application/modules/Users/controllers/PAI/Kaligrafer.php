22781ee06b7972e28d686e1b5c43c8bb8aea587e
maspriyambodo <maspriyambodo@gmail.com>
2021-01-13 18:36:35 +0700

    add module Users/PAI/kaligrafer

---
+++ application/modules/Users/controllers/PAI/Kaligrafer.php
@@ -0,0 +1,62 @@
+<?php
+
+defined('BASEPATH')OR exit('No direct script access allowed');
+/*
+ * Product:        System of kementerian agama Republik Indonesia
+ * License Type:   Government
+ * Access Type:    Multi-User
+ * License:        https://maspriyambodo.com
+ * maspriyambodo@gmail.com
+ * 
+ * Thank you,
+ * maspriyambodo
+ */
+
+/**
+ * Description of Guru_ngaji
+ *
+ * @author centos
+ */
+class Kaligrafer extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('PAI/M_Simpenais');
+        $this->Authentication = $this->M_Simpenais->Auth();
+    }
+
+    public function index() {
+        $data = [
+            'title' => 'Data Kaligrafer | RUDABI SYSTEM OF KEMENAG RI',
+            'username' => $this->Authentication[0]->uname,
+            'data' => read_file('https://simas.kemenag.go.id/rudabi/datapi/simpenaiss/kaligrafer?KEY=BOBA')
+        ];
+        $data['content'] = $this->parser->parse('Users/u_pai/V_Kaligrafer', $data, true);
+        return $this->parser->parse('Users/u_pai/Template', $data);
+    }
+
+    public function Provinsi() {
+        $param = $this->bodo->Url($this->input->post_get('key')); // output $param = Array ( [0] => 18 as province_id [1] => Bali as province_title)
+        $data = [
+            'title' => 'Data Kaligrafer Provinsi ' . $param[1] . '| RUDABI SYSTEM OF KEMENAG RI',
+            'username' => $this->Authentication[0]->uname,
+            'data' => read_file('https://simas.kemenag.go.id/rudabi/datapi/simpenaiss/kaligrafer?KEY=BOBA&province_id=' . $param[0]),
+            'param' => $param
+        ];
+        $data['content'] = $this->parser->parse('Users/u_pai/V_Kaligraferprov', $data, true);
+        return $this->parser->parse('Users/u_pai/Template', $data);
+    }
+
+    public function Kabupaten() {
+        $param = $this->bodo->Url($this->input->post_get('key')); // output $param = Array ( [0] => 18 as province_id [1] => Bali as province_title [2] => 245 as city_id [3] => Buleleng ascity_title)
+        $data = [
+            'title' => 'Data Kaligrafer ' . $param[1] . '| RUDABI SYSTEM OF KEMENAG RI',
+            'username' => $this->Authentication[0]->uname,
+            'data' => read_file('https://simas.kemenag.go.id/rudabi/datapi/simpenaiss/kaligrafer?KEY=BOBA&city_id=' . $param[2]),
+            'param' => $param
+        ];
+        $data['content'] = $this->parser->parse('Users/u_pai/V_Kaligraferkab', $data, true);
+        return $this->parser->parse('Users/u_pai/Template', $data);
+    }
+
+}
---
+++ application/modules/Users/views/u_pai/V_Kaligraferkab.php
@@ -0,0 +1,96 @@
+<?php $a = json_decode($data); ?>
+<div class="subheader py-2 py-lg-4 subheader-solid" id="kt_subheader">
+    <div class="container-fluid d-flex align-items-center justify-content-between flex-wrap flex-sm-nowrap">
+        <div class="d-flex align-items-center flex-wrap mr-2">
+            <h5 class="text-dark font-weight-bold mt-2 mb-2 mr-5 text-uppercase">Data Kaligrafer <?php echo $param[3]; ?></h5>
+        </div>
+    </div>
+</div>
+<div class="card card-custom" data-card="true" id="kt_card_1">
+    <div class="card-header">
+        <div class="card-title">
+            <a href="<?= base_url('Users/PAI/Kaligrafer/Provinsi?key=' . str_replace(['+', '/', '='], ['-', '_', '~'], $this->encryption->encrypt('?a=' . $param[0] . '&b=' . $param[1]))); ?>" class="btn btn-light btn-shadow-hover"><i class="fas fa-arrow-left"></i> Kembali</a>
+        </div>
+        <div class="card-toolbar">
+            <a href="#" class="btn btn-icon btn-sm btn-hover-light-primary mr-1" data-card-tool="toggle" data-toggle="tooltip" data-placement="top" title="Minimalkan">
+                <i class="ki ki-arrow-down icon-nm"></i>
+            </a>
+        </div>
+    </div>
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-bordered table-hover table-striped" style="width:100%;">
+                <thead class="text-center text-uppercase">
+                    <tr>
+                        <th rowspan="2">nama</th>
+                        <th colspan="3">lahir</th>
+                        <th rowspan="2">ayah</th>
+                        <th rowspan="2">ibu</th>
+                        <th rowspan="2">pasangan</th>
+                        <th rowspan="2">jenis kelamin</th>
+                        <th rowspan="2">alamat</th>
+                        <th rowspan="2">status</th>
+                        <th rowspan="2">pendidikan</th>
+                        <th rowspan="2">pekerjaan</th>
+                    </tr>
+                    <tr>
+                        <th>tempat</th>
+                        <th>tanggal</th>
+                        <th>usia</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($a as $b) { ?>
+                        <tr>
+                            <td><?php echo $b->kaligrafer_nama; ?></td>
+                            <td><?php echo $b->kaligrafer_tempat_lahir; ?></td>
+                            <td class="text-center"><?php echo $b->kaligrafer_tgl_lahir; ?></td>
+                            <td class="text-center">
+                                <?php
+                                $date = new DateTime($b->kaligrafer_tgl_lahir);
+                                $now = new DateTime();
+                                $interval = $now->diff($date);
+                                echo $interval->y;
+                                ?>
+                            </td>
+                            <td class="text-center"><?php echo $b->kaligrafer_nama_ayah; ?></td>
+                            <td class="text-center"><?php echo $b->kaligrafer_nama_ibu; ?></td>
+                            <td class="text-center"><?php echo $b->kaligrafer_nama_istri_suami; ?></td>
+                            <td class="text-center"><?php echo $b->kaligrafer_jenis_kelamin; ?></td>
+                            <td class="text-center"><?php echo $b->kaligrafer_alamat; ?></td>
+                            <td class="text-center"><?php echo $b->kaligrafer_status_kawin; ?></td>
+                            <td class="text-center"><?php echo $b->kaligrafer_pendidikan; ?></td>
+                            <td class="text-center"><?php echo $b->kaligrafer_pekerjaan; ?></td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<script>
+    window.onload = function () {
+        $('table').dataTable({
+            "ServerSide": true,
+            "order": [[0, "asc"]],
+            "paging": true,
+            "ordering": true,
+            "info": true,
+            "processing": false,
+            "deferRender": true,
+            "scrollCollapse": true,
+            "scrollX": true,
+            "scrollY": "400px",
+            dom: `<'row'<'col-sm-6 text-left'f><'col-sm-6 text-right'B>>
+                <'row'<'col-sm-12'tr>>
+                <'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7 dataTables_pager'lp>>`,
+            buttons: [
+                {extend: 'print', footer: true},
+                {extend: 'copyHtml5', footer: true},
+                {extend: 'excelHtml5', footer: true},
+                {extend: 'csvHtml5', footer: true},
+                {extend: 'pdfHtml5', footer: true}
+            ]
+        });
+    };
+</script>
