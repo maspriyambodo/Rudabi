9bf88a8ec42e46434415606fcfd379496f853cb6
maspriyambodo <maspriyambodo@gmail.com>
2021-01-13 18:49:41 +0700

    add module Users/PAI/dakwah

---
+++ application/modules/Users/controllers/PAI/Dakwah.php
@@ -0,0 +1,62 @@
+<?php
+
+defined('BASEPATH')OR exit('No direct script access allowed');
+/*
+ * Product:        System of kementerian agama Republik Indonesia
+ * License Type:   Government
+ * Access Type:    Multi-User
+ * License:        https://maspriyambodo.com
+ * maspriyambodo@gmail.com
+ * 
+ * Thank you,
+ * maspriyambodo
+ */
+
+/**
+ * Description of Lptq
+ *
+ * @author centos
+ */
+class Dakwah extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('PAI/M_Simpenais');
+        $this->Authentication = $this->M_Simpenais->Auth();
+    }
+
+    public function index() {
+        $data = [
+            'title' => 'Lembaga Dakwah | RUDABI SYSTEM OF KEMENAG RI',
+            'username' => $this->Authentication[0]->uname,
+            'data' => read_file('https://simas.kemenag.go.id/rudabi/datapi/simpenaiss/dakwah?KEY=BOBA')
+        ];
+        $data['content'] = $this->parser->parse('Users/u_pai/V_dakwah', $data, true);
+        return $this->parser->parse('Users/u_pai/Template', $data);
+    }
+
+    public function Provinsi() {
+        $param = $this->bodo->Url($this->input->post_get('key')); // output $param = Array ( [0] => 5 as province_id [1] => Jambi as province_title)
+        $data = [
+            'title' => 'Lembaga Dakwah Provinsi' . $param[1] . '| RUDABI SYSTEM OF KEMENAG RI',
+            'username' => $this->Authentication[0]->uname,
+            'data' => read_file('https://simas.kemenag.go.id/rudabi/datapi/simpenaiss/dakwah?KEY=BOBA&province_id=' . $param[0]),
+            'param' => $param
+        ];
+        $data['content'] = $this->parser->parse('Users/u_pai/V_dakwahprov', $data, true);
+        return $this->parser->parse('Users/u_pai/Template', $data);
+    }
+
+    public function Kabupaten() {
+        $param = $this->bodo->Url($this->input->post_get('key')); // output $param = Array ( [0] => 5 as province_id [1] => Jambi as province_title [2] => 190 as city_id [3] => Bantul as city_title)
+        $data = [
+            'title' => 'Lembaga Dakwah ' . $param[1] . '| RUDABI SYSTEM OF KEMENAG RI',
+            'username' => $this->Authentication[0]->uname,
+            'data' => read_file('https://simas.kemenag.go.id/rudabi/datapi/simpenaiss/dakwah?KEY=BOBA&city_id=' . $param[2]),
+            'param' => $param
+        ];
+        $data['content'] = $this->parser->parse('Users/u_pai/V_dakwahkab', $data, true);
+        return $this->parser->parse('Users/u_pai/Template', $data);
+    }
+
+}
--- application/modules/Users/views/u_pai/V_dakwahprov.php
+++ application/modules/Users/views/u_pai/V_dakwahprov.php
@@ -20,7 +20,7 @@ $k = 0; // jum_pngrs_perempuan
 <div class="card card-custom" data-card="true" id="kt_card_1">
     <div class="card-header">
         <div class="card-title">
-            <a href="<?= base_url('PAI/Dakwah/index/'); ?>" class="btn btn-light btn-shadow-hover"><i class="fas fa-arrow-left"></i> Kembali</a>
+            <a href="<?= base_url('Users/PAI/Dakwah/index/'); ?>" class="btn btn-light btn-shadow-hover"><i class="fas fa-arrow-left"></i> Kembali</a>
         </div>
         <div class="card-toolbar">
             <a href="#" class="btn btn-icon btn-sm btn-hover-light-primary mr-1" data-card-tool="toggle" data-toggle="tooltip" data-placement="top" title="Minimalkan">
@@ -113,7 +113,7 @@ $k = 0; // jum_pngrs_perempuan
                         $k += $b->jum_pngrs_perempuan; // jum_pngrs_perempuan
                         ?>
                         <tr>
-                            <td style="text-align: left !important;"><?php echo '<a href="' . base_url('PAI/Dakwah/Kabupaten?key=' . str_replace(['+', '/', '='], ['-', '_', '~'], $this->encryption->encrypt('?a=' . $param[0] . '&b=' . $param[1] . '&c=' . $b->city_id . '&d=' . $b->city_title))) . '" title="Detail ' . $b->city_title . '">' . $b->city_title . '</a>'; ?></td>
+                            <td style="text-align: left !important;"><?php echo '<a href="' . base_url('Users/PAI/Dakwah/Kabupaten?key=' . str_replace(['+', '/', '='], ['-', '_', '~'], $this->encryption->encrypt('?a=' . $param[0] . '&b=' . $param[1] . '&c=' . $b->city_id . '&d=' . $b->city_title))) . '" title="Detail ' . $b->city_title . '">' . $b->city_title . '</a>'; ?></td>
                             <td><?php echo number_format($b->jum_dakwah); ?></td>
                             <td><?php echo number_format($b->milik_sekolah); ?></td>
                             <td><?php echo number_format($b->milik_wakaf); ?></td>
