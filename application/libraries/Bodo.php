754eeb208b650069c1a84ec60c8cf6e902992971
maspriyambodo <maspriyambodo@gmail.com>
2021-01-18 16:24:43 +0700

    update libraries bodo

--- application/libraries/Bodo.php
+++ application/libraries/Bodo.php
@@ -26,18 +26,7 @@ class Bodo {
     }
 
     public function Curel($url) {
-        // persiapkan curl
-        $ch = curl_init();
-        // set url 
-        curl_setopt($ch, CURLOPT_URL, $url);
-        // return the transfer as a string 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        // $output contains the output string 
-        $output = curl_exec($ch);
-        // tutup curl 
-        curl_close($ch);
-        // menampilkan hasil curl
-        echo $output;
+        $output = read_file($url);
         return $output;
     }
 
