26ff8f7c43fc424db9862c4fc41609d72faa3e1b
maspriyambodo <maspriyambodo@gmail.com>
2021-01-22 16:03:35 +0700

    penambahan tampilan sekertariat

--- application/modules/Users/views/u_sekretariat/V_Dashboard.php
+++ application/modules/Users/views/u_sekretariat/V_Dashboard.php
@@ -6,51 +6,18 @@
         </div>
         <div class="card-body bg-white col-11 col-lg-12 col-xxl-10 mx-auto">
             <div class="row">
-                <div class="col">
-                    <a href="<?php echo base_url('Users/Sekretariat/Satker/index/'); ?>" class="card card-custom bg-danger bg-hover-state-danger card-stretch gutter-b">
+                <!-- <div class="col">
+                    <a href="<?php //echo base_url('Users/Sekretariat/Approved/index?key=' . str_replace(['+', '/', '='], ['-', '_', '~'], $this->encryption->encrypt('?a=' . date("Y"))) . ''); ?>" class="card card-custom bg-danger bg-hover-state-danger card-stretch gutter-b">
                         <div class="card-body">
                             <span class="svg-icon svg-icon-white svg-icon-3x ml-n1">
-                                <i class="flaticon-map" style="font-size: 48px;color: white;"></i> <b style="font-size: 30px;color: white;margin-left: 10px;"><?php echo number_format($data['satker'][0]->satker); ?></b>
-                            </span>
-                            <div class="font-weight-bold text-inverse-danger" style="margin: 5px 0px;font-size: 15px;">Data Satker</div>
-                        </div>
-                    </a>
-                </div>
-                <div class="col">
-                    <a href="<?php echo base_url('Users/Sekretariat/Usulan/index/' . str_replace(['+', '/', '='], ['-', '_', '~'], $this->encryption->encrypt(date("Y"))) . ''); ?>" class="card card-custom bg-primary bg-hover-state-primary card-stretch gutter-b">
-                        <div class="card-body">
-                            <span class="svg-icon svg-icon-white svg-icon-3x ml-n1">
-                                <i class="fas fa-chart-line" style="font-size: 48px;color: white;"></i> <b style="font-size: 30px;color: white;margin-left: 10px;"><?php echo number_format($data['satker'][1]->usulan_triwulan); ?></b>
-                            </span>
-                            <div class="font-weight-bold text-inverse-danger" style="margin: 5px 0px;font-size: 15px;">Usulan Triwulan</div>
-                        </div>
-                    </a>
-                </div>
-                <div class="col">
-                    <a href="<?php echo base_url('Users/Sekretariat/Input/index?key=' . str_replace(['+', '/', '='], ['-', '_', '~'], $this->encryption->encrypt('?a=' . date("Y"))) . ''); ?>" class="card card-custom bg-success bg-hover-state-success card-stretch gutter-b">
-                        <div class="card-body">
-                            <span class="svg-icon svg-icon-white svg-icon-3x ml-n1">
-                                <i class="fas fa-pencil-alt" style="font-size: 48px;color: white;"></i> <b style="font-size: 30px;color: white;margin-left: 10px;"><?php echo number_format($data['satker'][2]->input_triwulan); ?></b>
-                            </span>
-                            <div class="font-weight-bold text-inverse-danger" style="margin: 5px 0px;font-size: 15px;">Input Triwulan</div>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            
-            <div class="row">
-                <div class="col">
-                    <a href="<?php echo base_url('Users/Sekretariat/Approved/index?key=' . str_replace(['+', '/', '='], ['-', '_', '~'], $this->encryption->encrypt('?a=' . date("Y"))) . ''); ?>" class="card card-custom bg-danger bg-hover-state-danger card-stretch gutter-b">
-                        <div class="card-body">
-                            <span class="svg-icon svg-icon-white svg-icon-3x ml-n1">
-                                <i class="far fa-thumbs-up" style="font-size: 48px;color: white;"></i> <b style="font-size: 30px;color: white;margin-left: 10px;"><?php echo number_format($data['satker'][3]->approved_usulan); ?></b>
+                                <i class="far fa-thumbs-up" style="font-size: 48px;color: white;"></i> <b style="font-size: 30px;color: white;margin-left: 10px;"><?php //echo number_format($data['satker'][3]->approved_usulan); ?></b>
                             </span>
                             <div class="font-weight-bold text-inverse-danger" style="margin: 5px 0px;font-size: 15px;">Approved Usulan</div>
                         </div>
                     </a>
-                </div>
+                </div> -->
                 <div class="col">
-                    <a href="<?php echo base_url('Users/Sekretariat/Pegawai/index'); ?>" class="card card-custom bg-primary bg-hover-state-primary card-stretch gutter-b">
+                    <a href="<?php echo base_url('Users/Sekretariat/Pegawai/index'); ?>" class="card card-custom bg-success bg-hover-state-primary card-stretch gutter-b">
                         <div class="card-body">
                             <span class="svg-icon svg-icon-white svg-icon-3x ml-n1">
                                 <i class="fas fa-users" style="font-size: 48px;color: white;"></i> <b style="font-size: 30px;color: white;margin-left: 10px;"><?php echo number_format($data['sicakep'][0]->data_pegawai); ?></b>
@@ -60,12 +27,108 @@
                     </a>
                 </div>
                 <div class="col">
-                    <a href="<?php echo base_url('Users/Sekretariat/Pensiun/index'); ?>" class="card card-custom bg-success bg-hover-state-success card-stretch gutter-b">
+                    <a href="<?php echo base_url('Users/Sekretariat/Pegawai/index'); ?>" class="card card-custom bg-success bg-hover-state-primary card-stretch gutter-b">
+                        <div class="card-body">
+                            <span class="svg-icon svg-icon-white svg-icon-3x ml-n1">
+                                <i class="fas fa-male" style="font-size: 48px;color: white;"></i> <b style="font-size: 30px;color: white;margin-left: 10px;"><?php echo number_format($data['sicakep'][2]->laki_laki); ?></b>
+                            </span>
+                            <div class="font-weight-bold text-inverse-danger" style="margin: 5px 0px;font-size: 15px;">Pegawai Laki</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col">
+                    <a href="<?php echo base_url('Users/Sekretariat/Pegawai/index'); ?>" class="card card-custom bg-success bg-hover-state-primary card-stretch gutter-b">
+                        <div class="card-body">
+                            <span class="svg-icon svg-icon-white svg-icon-3x ml-n1">
+                                <i class="fas fa-female" style="font-size: 48px;color: white;"></i> <b style="font-size: 30px;color: white;margin-left: 10px;"><?php echo number_format($data['sicakep'][3]->wanita); ?></b>
+                            </span>
+                            <div class="font-weight-bold text-inverse-danger" style="margin: 5px 0px;font-size: 15px;">Pegawai Wanita</div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col">
+                    <a href="<?php echo base_url('Users/Sekretariat/Satker/index/'); ?>" class="card card-custom bg-info bg-hover-state-danger card-stretch gutter-b">
+                        <div class="card-body">
+                            <span class="svg-icon svg-icon-white svg-icon-3x ml-n1">
+                                <i class="flaticon-map" style="font-size: 48px;color: white;"></i> <b style="font-size: 30px;color: white;margin-left: 10px;"><?php echo number_format($data['satker'][0]->satker); ?></b>
+                            </span>
+                            <div class="font-weight-bold text-inverse-danger" style="margin: 5px 0px;font-size: 15px;">Data Satker</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col">
+                    <a href="<?php echo base_url('Users/Sekretariat/Usulan/index/' . str_replace(['+', '/', '='], ['-', '_', '~'], $this->encryption->encrypt(date("Y"))) . ''); ?>" class="card card-custom bg-info bg-hover-state-primary card-stretch gutter-b">
+                        <div class="card-body">
+                            <span class="svg-icon svg-icon-white svg-icon-3x ml-n1">
+                                <i class="fas fa-chart-line" style="font-size: 48px;color: white;"></i> <b style="font-size: 30px;color: white;margin-left: 10px;"><?php echo number_format($data['satker'][1]->usulan_triwulan); ?></b>
+                            </span>
+                            <div class="font-weight-bold text-inverse-danger" style="margin: 5px 0px;font-size: 15px;">Usulan Triwulan</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col">
+                    <a href="<?php echo base_url('Users/Sekretariat/Input/index?key=' . str_replace(['+', '/', '='], ['-', '_', '~'], $this->encryption->encrypt('?a=' . date("Y"))) . ''); ?>" class="card card-custom bg-info bg-hover-state-success card-stretch gutter-b">
+                        <div class="card-body">
+                            <span class="svg-icon svg-icon-white svg-icon-3x ml-n1">
+                                <i class="fas fa-pencil-alt" style="font-size: 48px;color: white;"></i> <b style="font-size: 30px;color: white;margin-left: 10px;"><?php echo number_format($data['satker'][2]->input_triwulan); ?></b>
+                            </span>
+                            <div class="font-weight-bold text-inverse-danger" style="margin: 5px 0px;font-size: 15px;">Input Triwulan</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col">
+                    <a href="<?php echo base_url('Users/Sekretariat/Approved/index?key=' . str_replace(['+', '/', '='], ['-', '_', '~'], $this->encryption->encrypt('?a=' . date("Y"))) . ''); ?>" class="card card-custom bg-info bg-hover-state-danger card-stretch gutter-b">
+                        <div class="card-body">
+                            <span class="svg-icon svg-icon-white svg-icon-3x ml-n1">
+                                <i class="far fa-thumbs-up" style="font-size: 48px;color: white;"></i> <b style="font-size: 30px;color: white;margin-left: 10px;"><?php echo number_format($data['satker'][3]->approved_usulan); ?></b>
+                            </span>
+                            <div class="font-weight-bold text-inverse-danger" style="margin: 5px 0px;font-size: 15px;">Approved Usulan</div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col">
+                    <a href="<?php echo base_url('Users/Sekretariat/Pensiun/index'); ?>" class="card card-custom bg-warning bg-hover-state-success card-stretch gutter-b">
                         <div class="card-body">
                             <span class="svg-icon svg-icon-white svg-icon-3x ml-n1">
                                 <i class="fas fa-house-user" style="font-size: 48px;color: white;"></i> <b style="font-size: 30px;color: white;margin-left: 10px;"><?php echo number_format($data['sicakep'][1]->data_pensiun); ?></b>
                             </span>
                             <div class="font-weight-bold text-inverse-danger" style="margin: 5px 0px;font-size: 15px;">Data Pensiun</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col">
+                    <a href="<?php echo base_url('Users/Sekretariat/Pensiun/index'); ?>" class="card card-custom bg-warning bg-hover-state-success card-stretch gutter-b">
+                        <div class="card-body">
+                            <span class="svg-icon svg-icon-white svg-icon-3x ml-n1">
+                                <i class="fas fa-address-card" style="font-size: 48px;color: white;"></i> <b style="font-size: 30px;color: white;margin-left: 10px;"><?php echo number_format($data['sicakep'][5]->I); ?></b>
+                            </span>
+                            <div class="font-weight-bold text-inverse-danger" style="margin: 5px 0px;font-size: 15px;">Pensiun Golongan I</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col">
+                    <a href="<?php echo base_url('Users/Sekretariat/Pensiun/index'); ?>" class="card card-custom bg-warning bg-hover-state-success card-stretch gutter-b">
+                        <div class="card-body">
+                            <span class="svg-icon svg-icon-white svg-icon-3x ml-n1">
+                                <i class="fas fa-address-card" style="font-size: 48px;color: white;"></i> <b style="font-size: 30px;color: white;margin-left: 10px;"><?php echo number_format($data['sicakep'][6]->III); ?></b>
+                            </span>
+                            <div class="font-weight-bold text-inverse-danger" style="margin: 5px 0px;font-size: 15px;">Pensiun Golongan III</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col">
+                    <a href="<?php echo base_url('Users/Sekretariat/Pensiun/index'); ?>" class="card card-custom bg-warning bg-hover-state-success card-stretch gutter-b">
+                        <div class="card-body">
+                            <span class="svg-icon svg-icon-white svg-icon-3x ml-n1">
+                                <i class="fas fa-address-card" style="font-size: 48px;color: white;"></i> <b style="font-size: 30px;color: white;margin-left: 10px;"><?php echo number_format($data['sicakep'][7]->IV); ?></b>
+                            </span>
+                            <div class="font-weight-bold text-inverse-danger" style="margin: 5px 0px;font-size: 15px;">Data Pensiun IV</div>
                         </div>
                     </a>
                 </div>
